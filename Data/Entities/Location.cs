bedfbe7a1557a171a725f2d897c29f4e67b11981
Wyatt Hunter <wyatthunter2102@gmail.com>
2025-10-17 09:38:02 -0500

    Seeder, still no DB yet

--- Data/Entities/Location.cs
+++ Data/Entities/Location.cs
@@ -1,30 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Entities
+public class Location
 {
-    public class Location
-    {
-        [Key]
-        public int ID { get; set; }
+    [Key]
+    public int ID { get; set; }
 
+    [Required, MaxLength(256)]
+    public string? Name { get; set; }
+
+    [Required, MaxLength(256)]
+    public string? Address { get; set; }
         [Required, MaxLength(256)]
-        public string? Name { get; set; }
-
+    public string? City { get; set; }
         [Required, MaxLength(256)]
-        public string? Address { get; set; }
-         [Required, MaxLength(256)]
-        public string? City { get; set; }
-         [Required, MaxLength(256)]
-        public string? State { get; set; }
-         [Required, MaxLength(256)]
-        public string? ZipCode { get; set; }
+    public string? State { get; set; }
+        [Required, MaxLength(256)]
+    public string? ZipCode { get; set; }
 
-        // Relationships
-        public ICollection<User> Users { get; set; } = new List<User>();
-    }
-
+    // Relationships
+    public ICollection<User> Users { get; set; } = new List<User>();
 }
--- Data/Entities/User.cs
+++ Data/Entities/User.cs
@@ -1,35 +1,27 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.Routing;
-using Entities;
-namespace Entities
+
+public class User
 {
-    public class User
-    {
-        [Key]
-        public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-        [Required, MaxLength(256)]
-        public string Name { get; set; } = string.Empty;
+    [Required, MaxLength(256)]
+    public string Name { get; set; } = string.Empty;
 
-        [Required, MaxLength(12)]
-        public string Phone { get; set; } = string.Empty;
+    [Required, MaxLength(12)]
+    public string Phone { get; set; } = string.Empty;
 
-        [Required, MaxLength(256)]
-        public string Email { get; set; } = string.Empty;
+    [Required, MaxLength(256)]
+    public string Email { get; set; } = string.Empty;
 
-        [Required, MaxLength(128)]
-        public String Role { get; set; } = string.Empty;  // Change to ICollection?
+    [Required, MaxLength(128)]
+    public String Role { get; set; } = string.Empty;  // Change to ICollection?
 
-        // Navigation property for UserStation experience
-        public ICollection<String>? StationsWorked { get; set; }
+    // Navigation property for UserStation experience
+    public ICollection<String>? StationsWorked { get; set; }
 
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
-        public int LocationId { get; set; }
-    }
+    public int LocationId { get; set; }
 }
