d20bd574c7cbb83ff2de2f699346191c4e633740
rrrockey <rockey.ryan@yahoo.com>
2025-11-26 15:37:42 -0600

    feat: add UPDATE funcitonality to locations

--- Models/LocationModel.cs
+++ Models/LocationModel.cs
@@ -2,14 +2,14 @@ namespace CulinaryCommand.Models
 {
     public class LocationModel
     {
-        public int? Id { get; set; } = null;
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
         public string State { get; set; } = string.Empty;
         public string ZipCode { get; set; } = string.Empty;
 
-        public string CompanyId { get; set; } = string.Empty;
+        public int CompanyId { get; set; }
         public string MarginEdgeKey { get; set; } = string.Empty;
     }
 }
