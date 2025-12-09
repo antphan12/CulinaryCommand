454fa1ac497cb8a3479e5f4c325da65ed13bbc23
Kevin Tran <kevtran04@gmail.com>
2025-12-08 18:31:13 -0600

    feat: add caching + playwright tests
    also adding playwright screenshots

---
+++ PlaywrightTests/GenerateTestScreenshots.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightTests;
+
+public class GenerateTestScreenshots
+{
+    [Fact]
+    public async Task home_page_screenshot()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = true
+        });
+
+        var page = await browser.NewPageAsync();
+
+        await page.GotoAsync("http://localhost:5000");
+
+        var artifactsDir = Path.Combine("CulinaryCommandApp", "playwright-artifacts", "screenshots");
+        Directory.CreateDirectory(artifactsDir);
+
+        var screenshotPath = Path.Combine(artifactsDir, "home-page.png");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = screenshotPath,
+            FullPage = true
+        });
+    }
+}
